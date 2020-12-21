cca771eee12f34411ba1755b8616dbe34675ea90
Ibidi <7294164+1b1d1@users.noreply.github.com>
2020-12-21 15:19:19 -0600

    Updated the code to have variables and strings. For loop does not work and needs more if statements for each option

--- script.js
+++ script.js
@@ -1,23 +1,74 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVVWXYZ";
+var number = "0123456789";
+var special = "!@#$%^&*()_+-=?><";
+console.log(lower)
+ 
 
-function generatePassword() {
-  var length = prompt("How many characters do you want in your passwod? (8-128)");
-  if (length = 
-  var lowerSet = comfirm("Do you want lowercase characters in your password? (Confirm = yes, Cancel = no)");
-  var upperSet = confirm("Do you want special characters in your password? (Confirm = yes, Cancel = no)");
+function getPassOptions() {
+  var length = prompt("How many characters do you want in your password? (8-128)");
+
+  if(isNaN(length) === true) {
+    alert('pass length should be a number')
+    return;
+  }
+  if(length < 8) {
+    alert('length must be greater than 8')
+    return;
+  }
+
+  if(length > 128) {
+    alert('length must be smaller than 128')
+  }
+
+  var lowerSet = confirm("Do you want lowercase characters in your password? (Confirm = yes, Cancel = no)");
+  var upperSet = confirm("Do you want uppercase characters in your password? (Confirm = yes, Cancel = no)");
+  var numberSet = confirm ("Do you want numbers in your password (Ok = yes, Cancel = No)");
   var specialSet = confirm("Do you want special characters in your password? (Confim = Yes, Cancel = No)");
   
+  var passwordOptions = {
+    length:length,
+    hasLower: lowerSet,
+    hasUpper: upperSet ,
+    hasNumber: numberSet,
+    hasSpecial: specialSet
+  }
+  console.log(passwordOptions)
 
-//       retVal = "";
-//   for (var i = 0, n = charset.length; i < length; ++i) {
-//       retVal += charset.charAt(Math.floor(Math.random() * n));
-//   }
-//   return retVal;
+
+   return passwordOptions;
+ }
+
+ function generatePassword() {
+   var options = getPassOptions();
+   var result = ''; // where we save the password to
+   var possibleChoices = '';
+   //
+   var guaranteedCharacters = '';
+
+   if(options.hasLower === true) {
+      
+      possibleChoices = possibleChoices.concat(lower)
+      guaranteedCharacters = guaranteedCharacters.concat(getRandomChar(lower))
+
+   }
+   console.log(possibleChoices)
+  for (var i = 0, n = options.length; i < length; ++i) {
+    // google how to add a  charcter to the end of a string
+     result = result.concat( getRandomChar(possibleChoices))
+  }
+  console.log('result: ', result)
+  return result
+ }
+
+ function getRandomChar(str) {
+    var randomIndex = Math.floor(Math.random() * str.length);
+    return str[randomIndex]
  }
 
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
